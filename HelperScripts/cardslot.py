4bddfe6c5487b859050c46d966d2d3c41fae01d5
amangupta20 <58002413+amangupta20@users.noreply.github.com>
2025-09-08 19:02:44 +0530

    Reapply "template matching to slot mapping"
    This reverts commit d7c8985588259b2ae7ba6c6a2e76cb4f97613019.

---
+++ HelperScripts/cardslot.py
@@ -0,0 +1,11 @@
+def which_slot(tup):
+    print(tup[0], tup[1])
+    if tup[0] >0 and tup[0] < 40:
+        return 1
+    elif tup[0] > 40 and tup[0] < 80:
+        return 2
+    elif tup[0] > 80 and tup[0] < 120:
+        return 3
+    elif tup[0] > 120 and tup[0] < 160:
+        return 4
+    pass
