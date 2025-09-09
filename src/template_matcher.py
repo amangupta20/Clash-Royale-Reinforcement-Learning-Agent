59db673b6d69190137bc60303dcf6d0a8274185b
amangupta20 <58002413+amangupta20@users.noreply.github.com>
2025-09-09 22:29:02 +0530

    working on live detection

--- src/template_matcher.py
+++ src/template_matcher.py
@@ -15,17 +15,30 @@ class DeckMatcher:
                 templates.append((card, template))
         return templates
     def detect_slots(self, frame):
-        game_image = frame
+
+        game_image = self.crop(frame)
          # Dictionary to hold detected cards in slots
         detected_slots = dict((i, None) for i in range(1, 5))
         for card in self.templates:
             result = cv.matchTemplate(game_image, card[1], cv.TM_CCOEFF_NORMED)
             min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-            threshold = 0.6
+            threshold = 0.8
             if max_val >= threshold:
+                print(f"Detected {card[0]} with confidence {max_val} at location {max_loc}")
+                 # Determine which slot the card belongs to 
                 slot = cardslot.which_slot(max_loc)
-                if detected_slots[slot] is None:
+                if slot is not None and detected_slots[slot] is None:
                     detected_slots[slot] = card[0]
-
-
-        return detected_slots
+        def crop(frame):
+            CROP_LEFT = 657
+            CROP_RIGHT = 657
+            TARGET_WIDTH = 360
+            TARGET_HEIGHT = 110
+            CROP_TOP=500
+            CROP_BOT=100
+            height, width = frame.shape[:2]
+            cropped_image = frame[CROP_TOP:height-CROP_BOT, CROP_LEFT:width-CROP_RIGHT]
+            cv.imwrite("cropped.png", cropped_image)
+            return cropped_image
+        
+            return detected_slots
